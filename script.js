206a7d3f27aba6f8a2cb02896307853994e2c471
buenonascimentoana-cpu <bueno.nascimento.ana@escola.pr.gov.br>
2026-09-09 17:27:52 -0300

    atualizei meu codigo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const botao = document.querySelector("botaoMensagem");
+const mensagem = document.querySelector("mensagemInterativa");
+    botao.addEventListener("click", mostraMensagem);
+    function mostrarMensagem() {
+        mensagem.textContent =
+           "Vocẽ acabou de criar uma interação com JavaScripti"
 
+    }
